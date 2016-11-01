e47dc7c497c56c521962df6571e48feb21d94f6a
colbygatte <colbygatte@gmail.com>
2016-11-01 10:21:03 -0500

    Initial Commit

---
+++ UsingMaps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsingMaps
+//
+//  Created by Colby Gatte on 11/1/16.
+//  Copyright © 2016 colbyg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
