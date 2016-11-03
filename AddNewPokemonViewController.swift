1095fa8f3e821a24986abdb0ea2c43b785c6cd6f
colbygatte <colbygatte@gmail.com>
2016-11-02 22:51:22 -0500

    allows adding pokemon via push, doesnt work

---
+++ AddNewPokemonViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddNewPokemonViewController.swift
+//  UsingMaps
+//
+//  Created by Colby Gatte on 11/2/16.
+//  Copyright © 2016 colbyg. All rights reserved.
+//
+
+import UIKit
+
+class AddNewPokemonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
